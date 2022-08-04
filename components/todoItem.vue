c1dea5274e54906a508abaf928eac323706d08ff
Анвар <anvar@iMac-test.local>
2022-08-04 17:01:55 +0300

    input done

---
+++ components/todoItem.vue
@@ -0,0 +1,29 @@
+<template>
+                    <div class="todo-item">
+            <div class="todo-title">{{data.title}}</div>
+            <div class="todo-content">{{data.description}}</div>
+
+                <input type="checkbox" class="todo-checkbox" v-model="checked" />
+
+        </div>
+</template>
+<script>
+import { defineComponent,ref } from '@vue/composition-api'
+
+export default defineComponent({
+    name: 'todoItem',
+    props: {
+        data: {
+            type: Object,
+            default: null
+        }
+    },
+    setup(props,ctx) {
+        const checked = ref(props.data.done)
+
+        return {
+            checked
+            }
+    },
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,16 @@
     <div class="content-buttons">
         <button class="content-button">List</button>
         <button class="content-button">Cards</button>
-        <button class="add-button">Add</button>
+        <button class="add-button" @click="addTodo">Add</button>
+        <input type="text" class="todo-input" v-model="title" placeholder="Title"/>
+        <input type="text" class="todo-input" v-model="description" placeholder="Decription"/>
     </div>
     <div class="todo-list">
         <div class="todo-list-wrapper flex">
             <div class="todos">
-                <div class="todo-item">
-            <div class="todo-title">Title</div>
-            <div class="todo-content">Todo content</div>
-            <input type="text" class="todo-input">
-                <input type="checkbox" class="todo-checkbox" />
-
-        </div>
+               <template v-for="todo in todosListReactive" :key=" todo.id">
+                 <todo-item :data="todo"></todo-item>
+               </template>
     </div>
         </div>
     </div>
@@ -25,12 +23,59 @@
 
 </template>
 <script>
-import { defineComponent } from '@vue/composition-api'
-
+import { defineComponent,reactive,ref } from '@vue/composition-api'
+const todosList = [
+    {
+        id: 1,
+        title: 'vodolaz',
+        description: 'lere-lero-lero-lero-lero',
+        done: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+        {
+        id: 2,
+        title: 'Jojo',
+        description: 'Yes',
+        done: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }
+]
 export default defineComponent({
     name: 'home',
     setup() {
+        let count = 3
+        const title = ref('')
+        const description = ref('')
+        const todosListReactive = reactive([...todosList])
+        const addTodo = () => {
+        const item = {
+        id: count++,
+        title: title.value,
+        description: description.value,
+        done: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+        } 
+            if(todosListReactive.filter(item => item.id !== item.id)){
+                todosListReactive.push(item)
+                title.value = ''
+                description.value= ''
+                console.log('todoItem',item)
+                console.log('todosListReactive',todosListReactive)
+            } else {
+                return
+            }
+        }
         
+        return {
+            title,
+            description,
+            todosListReactive,
+            todosList,
+            addTodo
+        }
     },
 })
 </script>
@@ -45,19 +90,25 @@ body{
         margin-right: auto;
     }
 button{
-        border: none;
-        background-color: rgb(123, 0, 255) ;
-        cursor: pointer;
-        color: #fff;
-        border-radius: 8px;
-        padding: 5px 10px;
-        transition: all ease-in .2s;
-        &:active{
+    border: none;
+    background-color: rgb(123, 0, 255) ;
+    cursor: pointer;
+    color: #fff;
+    border-radius: 8px;
+    padding: 5px 10px;
+    transition: all ease-in .2s;
+
+    &:active{
             transform: scale(.9,.9);
     }
+
     &:hover{
         background-color: rgb(147, 47, 255);
     }
+}
+
+.btn-active{
+    background-color: rgb(147, 47, 255);
 }
 input {
     outline: none;
@@ -75,11 +126,32 @@ input[type=checkbox]{
     transition: all ease-in .2s;
     &:checked{
         background-color: rgb(123, 0, 255);
+        &:hover{
+        border: 4px solid #fff;
+        background-color: rgb(123, 0, 255);
+    }
+    }
+    &:hover{
+        border: 7px solid #fff;
+        background-color: rgb(123, 0, 255);
     }
 }
-.todo-list{
+.todo{
+    &-list{
         background-color: rgb(35, 35, 35);
         border-radius: 8px;
+        padding: 10px;
+}
+&-item{
+    background-color: rgb(132, 0, 255);
+    border-radius: 8px;
+    display: flex;
+    column-gap: 10px;
+    height: 50px;
+    align-items: center;
+    padding: 5px;
+    margin-top: 10px;
+}
 }
 .flex{
     display: flex;
