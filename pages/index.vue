7f9781baaba424acc719612ebcf4dcbfc4295a60
Анвар <anvar@iMac-test.local>
2022-08-04 11:39:15 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>Home</h1>
+<div class="container">
+    <h2>Content</h2>
+    <div class="todos">
+        <div class="todo-item">
+            <div class="todo-title">Title</div>
+            <div class="todo-content">Todo content</div>
+            <input type="text" class="todo-input">
+            <button class="todo-button">Add</button>
+            <button class="todo-button">Done</button>
+        </div>
+    </div>
+</div>
+
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    name: 'home',
+    setup() {
+        
+    },
+})
+</script>
+<style scoped>
+    .container{
+        width: 1080px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
---
+++ store/todos.ts
@@ -0,0 +1,61 @@
+import { defineStore } from "pinia";
+import { v4 as uuid } from 'uuid'
+
+export interface Todo {
+    id: string
+    title: string
+    done: boolean
+    createdAt: Date
+    updatedAt: Date
+}
+
+export interface TodoState {
+    items: Todo[] | undefined[]
+}
+const state = (): TodoState => ({
+    items: []
+})
+
+export interface TodoAdd {
+    title: string
+}
+
+export interface TodoUpdate {
+    title?: string,
+    done?: boolean
+}
+const getters = {
+    getById: (state: TodoState) => (id: string) => {
+        return state.items.find((item: Todo) => item.id === id)
+    },
+    getByDescTodos: (state: TodoState) => [...state.items].sort((a: Todo, b: Todo) => a.createdAt.getTime() - b.createdAt.getTime())
+}
+
+const actions = {
+    add(partialTodo: TodoAdd){
+        const todo: Todo = {
+            id: uuid(),
+            ...partialTodo,
+            done: false,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+        this.items.push(todo)
+    },
+    remove(id: string) {
+        this.items = this.items.filter( (todo: Todo) => todo.id !== id)
+    },
+    update(id: string, update: TodoUpdate){
+        const index = this.items.findIndex( item => item.id === id)
+        this.items[index] = {...this.items[index],
+            ...update,
+            upadatedAt: new Date()
+        }
+    }
+}
+
+export const useTodoStore = defineStore('todoStore',{
+    state,
+    getters,
+    actions
+})
